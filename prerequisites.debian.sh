fedfe8dde9ab42c02a4aa157361869d709f1b6aa
Markus Amsler <markus.amsler@eigenstrom.ch>
2024-06-24 07:14:14 +0200

    requisits: add debian bookworm packages

--- prerequisites.debian.sh
+++ prerequisites.debian.sh
@@ -4,10 +4,12 @@ apt-get install python3
 pip3 install PyQt5
 pip3 install jsonpickle
 pip3 install requests
-pip3 install pdfrw
 pip3 install selenium
 pip3 install qrcode
 pip3 install svgwrite
 pip3 install iso3166
 pip3 install stdnum
 pip3 install svglib
+
+# debian bookworm
+# apt install python3-pyqt5 python3-jsonpickle python3-requests python3-selenium python3-qrcode python3-svgwrite python3-iso3166 python3-stdnum python3-svglib
