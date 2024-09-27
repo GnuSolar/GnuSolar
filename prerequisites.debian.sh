6a1cd2c0abcded8dc7eb54a98dbe584ba4f8ee62
Amsler Solar <mail@markus-amsler.ch>
2024-09-27 15:23:48 +0200

    Dependendcies: add genshi

--- prerequisites.debian.sh
+++ prerequisites.debian.sh
@@ -10,6 +10,7 @@ pip3 install svgwrite
 pip3 install iso3166
 pip3 install stdnum
 pip3 install svglib
+pip3 install genshi
 
 # debian bookworm
-# apt install python3-pyqt5 python3-jsonpickle python3-requests python3-selenium python3-qrcode python3-svgwrite python3-iso3166 python3-stdnum python3-svglib
+# apt install python3-pyqt5 python3-jsonpickle python3-requests python3-selenium python3-qrcode python3-svgwrite python3-iso3166 python3-stdnum python3-svglib python3-gensdhi
